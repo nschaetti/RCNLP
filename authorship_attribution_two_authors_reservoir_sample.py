4055c6b74aebcb18e86bd1dfffa2559a7c9a82e1
Nils Schaetti <nils.schaetti@unine.ch>
2017-05-02 17:09:05 +0200

    Language and multidim mem language

--- authorship_attribution_two_authors_reservoir_sample.py
+++ authorship_attribution_two_authors_reservoir_sample.py
@@ -67,12 +67,14 @@ if __name__ == "__main__":
     parser.add_argument("--author1", type=str, help="Author 1' ID.")
     parser.add_argument("--author2", type=str, help="Author 2's ID.")
     parser.add_argument("--samples", type=int, help="Number of samples to use to assess accuracy.", default=20)
-    parser.add_argument("--training_size", type=int, help="Number of texts to train the model.", default=2)
+    parser.add_argument("--training-size", type=int, help="Number of texts to train the model.", default=2)
+    parser.add_argument("--test-size", type=int, help="Number of texts to assess the model.", default=20)
     parser.add_argument("--lang", type=str, help="Language (ar, en, es, pt)", default='en')
     parser.add_argument("--converter", type=str, help="The text converter to use (fw, pos, tag, wv).", default='pos')
     parser.add_argument("--pca-model", type=str, help="PCA model to load", default=None)
     parser.add_argument("--in-components", type=int, help="Number of principal component to reduce inputs to.",
                         default=-1)
+    parser.add_argument("--sentence", action='store_true', help="Test sentence classification rate?", default=False)
     args = parser.parse_args()
 
     # Logging
@@ -98,16 +100,15 @@ if __name__ == "__main__":
         converter = RCNLPWordVectorConverter(resize=args.in_components, pca_model=pca_model)
     # end if
 
-    # >> 3. Array for results
-    average_doc_success_rate = np.array([])
-    average_sen_success_rate = np.array([])
+    # >> 2. Array for results
+    average_success_rate = np.array([])
 
-    # For each samples
+    # >> 3. Prepare training and test set.
+    training_set_indexes = np.arange(0, args.training_size, 1)
+    test_set_indexes = np.arange(args.training_size, args.training_size + args.test_size, 1)
+
+    # >> 4. For each samples
     for s in range(0, args.samples):
-        # >> 5. Prepare training and test set.
-        training_set_indexes = np.arange(0, args.training_size, 1)
-        test_set_indexes = np.arange(args.training_size, 100, 1)
-
         # >> 6. Create Echo Word Classifier
         classifier = RCNLPEchoWordClassifier(size=rc_size, input_scaling=rc_input_scaling, leak_rate=rc_leak_rate,
                                              input_sparsity=rc_input_sparsity, converter=converter, n_classes=2,
@@ -125,60 +126,53 @@ if __name__ == "__main__":
         classifier.train()
 
         # >> 9. Test model performance
-        doc_success = sen_success = 0.0
-        doc_count = sen_count = 0.0
+        success = 0.0
+        count = 0.0
         for author_index, author_id in enumerate((args.author1, args.author2)):
             author_path = os.path.join(args.dataset, "total", author_id)
             for file_index in test_set_indexes:
                 file_path = os.path.join(author_path, str(file_index) + ".txt")
 
                 # Document success rate
-                author_pred, _, _ = classifier.pred(file_path)
-                if author_pred == author_index:
-                    doc_success += 1.0
-                # end if
-                doc_count += 1.0
-
-                # Sentence success rate
-                nlp = spacy.load(args.lang)
-                doc = nlp(io.open(file_path, 'r').read())
-                for sentence in doc.sents:
-                    sentence_pred, _, _ = classifier.pred_text(sentence.text)
-                    if sentence_pred == author_index:
-                        sen_success += 1.0
+                if not args.sentence:
+                    author_pred, _, _ = classifier.pred(file_path)
+                    if author_pred == author_index:
+                        success += 1.0
                     # end if
-                    sen_count += 1.0
-                # end for
+                    count += 1.0
+                else:
+                    # Sentence success rate
+                    nlp = spacy.load(args.lang)
+                    doc = nlp(io.open(file_path, 'r').read())
+                    for sentence in doc.sents:
+                        sentence_pred, _, _ = classifier.pred_text(sentence.text)
+                        if sentence_pred == author_index:
+                            success += 1.0
+                        # end if
+                        count += 1.0
+                    # end for
+                # end if
+
             # end for
         # end for
 
         # >> 10. Log success
-        logging.save_results("Document success rate ", (doc_success / doc_count) * 100.0, display=True)
-        logging.save_results("Sentence success rate ", (sen_success / sen_count) * 100.0, display=True)
+        logging.save_results("Success rate ", (success / count) * 100.0, display=True)
 
         # >> 11. Save results
-        average_doc_success_rate = np.append(average_doc_success_rate, [(doc_success / doc_count) * 100.0])
-        average_sen_success_rate = np.append(average_sen_success_rate, [(sen_success / sen_count) * 100.0])
+        average_success_rate = np.append(average_success_rate, [(success / count) * 100.0])
 
         # Delete variables
         del classifier
     # end for
 
     # Log results
-    logging.save_results("Doc. average success rate ", np.average(average_doc_success_rate), display=True)
-    logging.save_results("Doc. success rate std ", np.std(average_doc_success_rate), display=True)
-    logging.save_results("Sen. average success rate ", np.average(average_sen_success_rate), display=True)
-    logging.save_results("Sen. success rate std ", np.std(average_sen_success_rate), display=True)
+    logging.save_results("Average success rate ", np.average(average_success_rate), display=True)
+    logging.save_results("Success rate std ", np.std(average_success_rate), display=True)
 
     # Plot histogram
     bins = np.linspace(0, 1.0, 100)
-    plt.hist(average_doc_success_rate, bins, label="Doc. success rate distribution")
-    plt.legend(loc='upper right')
-    plt.show()
-
-    # Plot histogram
-    bins = np.linspace(0, 1.0, 100)
-    plt.hist(average_sen_success_rate, bins, label="Sen. success rate distribution")
+    plt.hist(average_success_rate, bins, label="Doc. success rate distribution")
     plt.legend(loc='upper right')
     plt.show()
 
