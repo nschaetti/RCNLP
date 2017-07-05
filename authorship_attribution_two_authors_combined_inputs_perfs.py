b04170f6a059607397b3359787ac03a15961499e
nschaetti <n.schaetti@gmail.com>
2017-07-05 18:04:26 +0200

    Plus

--- authorship_attribution_two_authors_combined_inputs_perfs.py
+++ authorship_attribution_two_authors_combined_inputs_perfs.py
@@ -221,12 +221,12 @@ if __name__ == "__main__":
                         # Log results
                         logging.save_results("Average success rate ", np.average(average_success_rate), display=True)
                         logging.save_results("Success rate std ", np.std(average_success_rate), display=True)
-                        if original_size_perf != average_success_rate:
+                        if (original_size_perf != average_success_rate).all():
                             logging.save_results("Paired t-test again original size ",
                                                  stats.ttest_rel(original_size_perf, average_success_rate).pvalue * 100,
                                                  display=True)
                         # end if
-                        if none_size_perf != average_success_rate:
+                        if (none_size_perf != average_success_rate).all():
                             logging.save_results("Paired t-test again first repr. alone",
                                                  stats.ttest_rel(none_size_perf, average_success_rate).pvalue * 100,
                                                  display=True)
