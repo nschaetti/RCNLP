cf7d1ff7c7ac946bc7bae97aa93a3914322c4799
nschaetti <n.schaetti@gmail.com>
2017-07-24 15:16:26 +0200

    Classifiers

--- core/classifiers/SL2GramTextClassifier.py
+++ core/classifiers/SL2GramTextClassifier.py
@@ -23,6 +23,9 @@
 #
 
 # Imports
+import spacy
+from sys import getsizeof
+import decimal
 from .TextClassifier import TextClassifier
 
 
@@ -30,28 +33,120 @@ from .TextClassifier import TextClassifier
 class SL2GramTextClassifier(TextClassifier):
 
     # Constructor
-    def __init__(self, classes):
+    def __init__(self, classes, smoothing, smoothing_param):
         """
         Constructor
-        :param classes: Classes
+        :param classes:
+        :param smoothing:
+        :param smoothing_param:
         """
         # Class super class
         super(SL2GramTextClassifier, self).__init__(classes=classes)
+
+        # Properties
+        self._n_token = 0
+        self._n_total_token = 0
+
+        # Init dictionaries
+        self._token_counters = dict()
+        self._class_counters = dict()
+
+        # Smoothing
+        self._smoothing = smoothing
+        self._smoothing_param = smoothing_param
     # end __init__
 
     ##############################################
     # Public
     ##############################################
 
+    # Get token count
+    def get_token_count(self):
+        """
+        Get token count
+        :return:
+        """
+        return len(self._token_counters.keys())
+    # end get_token_count
+
     # Train the model
-    def train(self, x, y):
+    def train(self, x, y, verbose=False):
         """
         Train
-        :param x: Example's inputs.
-        :param y: Example's outputs.
+        :param x: Example's inputs
+        :param y: Example's outputs
+        :param verbose: Verbosity
         """
-        pass
+        # Tokens
+        tokens = spacy.load('en')(x)
+
+        # For each token
+        for token in tokens:
+            token_text = token.text.lower()
+            # Token counters
+            try:
+                self._token_counters[token_text] += 1.0
+            except KeyError:
+                self._token_counters[token_text] = 1.0
+                self._n_token += 1.0
+            # end try
+
+            # Create entry in class counter
+            try:
+                probs = self._class_counters[token_text]
+            except KeyError:
+                self._class_counters[token_text] = dict()
+            # end try
+
+            # Class counters
+            if y in self._class_counters[token_text].keys():
+                self._class_counters[token_text][y] += 1.0
+            else:
+                self._class_counters[token_text][y] = 1.0
+            # end if
+
+            # One more token
+            self._n_total_token += 1.0
+        # end token
     # end train
+
+    ##############################################
+    # Private
+    ##############################################
+
+    # Get token probability
+    def __getitem__(self, item):
+        """
+        Get token probability
+        :param item:
+        :return:
+        """
+        # Probs
+        probs = dict()
+
+        # Set default
+        for c in self._classes:
+            try:
+                probs[c] = self._class_counters[item][c] / self._token_counters[item]
+            except KeyError:
+                probs[c] = 0.0
+                # end try
+        # end for
+        return probs
+    # end __getitem__
+
+    # To String
+    def __str__(self):
+        """
+        To string
+        :return:
+        """
+        return "SL2GramTextClassifier(n_classes={}, n_tokens={}, mem_size={}o, " \
+               "token_counters_mem_size={} Go, class_counters_mem_size={} Go, n_total_token={})" \
+            .format(self._n_classes, self.get_token_count(),
+                    getsizeof(self), round(getsizeof(self._token_counters) / 1073741824.0, 4),
+                    round(getsizeof(self._class_counters) / 1073741824.0, 4), self._n_total_token)
+    # end __str__
 
     ##############################################
     # Private
@@ -64,15 +159,105 @@ class SL2GramTextClassifier(TextClassifier):
         :param x: Document's text.
         :return: A tuple with found class and values per classes.
         """
-        pass
+        # Text's probabilities
+        text_probs = dict()
+
+        # Init
+        for c in self._classes:
+            text_probs[c] = decimal.Decimal(1.0)
+        # end for
+
+        # Parse text
+        text_tokens = spacy.load('en')(x)
+
+        # Get all tokens
+        tokens = list()
+        for token in text_tokens:
+            tokens.append(token)
+        # end for
+
+        # For each token
+        for token in tokens:
+            token_text = token.text.lower()
+
+            # Get token probs for each class
+            try:
+                token_probs = self[token_text]
+                collection_prob = self._token_counters[token_text] / self._n_total_token
+            except KeyError:
+                continue
+            # end try
+
+            # For each class
+            for c in self._classes:
+                smoothed_value = SL2GramTextClassifier.smooth(self._smoothing, token_probs[c], collection_prob,
+                                                              len(tokens),
+                                                              param=self._smoothing_param)
+                text_probs[c] *= decimal.Decimal(smoothed_value)
+                # end for
+        # end for
+
+        # Get highest prob
+        max = decimal.Decimal(0.0)
+        result_class = ""
+        for c in self._classes:
+            if text_probs[c] > max:
+                max = text_probs[c]
+                result_class = c
+                # end if
+        # end for
+
+        return result_class, text_probs
     # end _classify
 
-    # Finalize the training
-    def _finalize_training(self):
+    ##############################################
+    # Static
+    ##############################################
+
+    # Dirichlet prior smoothing function
+    @staticmethod
+    def smooth_dirichlet_prior(doc_prob, col_prob, doc_length, mu):
         """
-        Finalize training.
+        Dirichlet prior smoothing function
+        :param doc_prob:
+        :param col_prob:
+        :param doc_length:
+        :param mu:
+        :return:
         """
-        pass
-    # end _finalize_training
+        return (float(doc_length) / (float(doc_length) + float(mu))) * doc_prob + \
+               (float(mu) / (float(mu) + float(doc_length))) * col_prob
+    # end smooth
+
+    # Jelinek Mercer smoothing function
+    @staticmethod
+    def smooth_jelinek_mercer(doc_prob, col_prob, param_lambda):
+        """
+        Jelinek Mercer smoothing function
+        :param col_prob:
+        :param param_lambda:
+        :return:
+        """
+        return (1.0 - param_lambda) * doc_prob + param_lambda * col_prob
+    # end smooth
+
+    # Smoothing function
+    @staticmethod
+    def smooth(smooth_algo, doc_prob, col_prob, doc_length, param):
+        """
+        Smoothing function
+        :param smooth_algo: Algo type
+        :param doc_prob:
+        :param col_prob:
+        :param doc_length:
+        :param param:
+        :return:
+        """
+        if smooth_algo == "dp":
+            return SL2GramTextClassifier.smooth_dirichlet_prior(doc_prob, col_prob, doc_length, param)
+        else:
+            return SL2GramTextClassifier.smooth_jelinek_mercer(doc_prob, col_prob, param)
+        # end if
+    # end smooth
 
 # end SL2GramTextClassifier
