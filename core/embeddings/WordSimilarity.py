8e7ec08c32e35137392ae2ff6646bbaf5c4d36ff
nschaetti <n.schaetti@gmail.com>
2017-08-23 13:15:09 +0200

    Word similarities

--- core/embeddings/WordSimilarity.py
+++ core/embeddings/WordSimilarity.py
@@ -2,6 +2,18 @@
 
 # Word similarity abstract class
 class WordSimilarity(object):
+    """
+    Word similarity abstract class
+    """
+
+    # Constructor
+    def __init__(self):
+        """
+        Constructor
+        """
+        self._word_pos = 0
+        self._n_words = 0
+    # end __init__
 
     ########################################
     # Public
@@ -17,6 +29,48 @@ class WordSimilarity(object):
         """
         pass
     # end similarity
+
+    # Get similar words
+    def get_similar_words(self, word, limit=10):
+        """
+        Get similar words
+        :param word:
+        :param limit:
+        :return:
+        """
+        pass
+    # end get_similar_words
+
+    # Get list of words
+    def words(self):
+        """
+        Get list of words
+        :return:
+        """
+        pass
+    # end words
+
+    ########################################
+    # Override
+    ########################################
+
+    # Iterator
+    def __iter__(self):
+        """
+        Iterator
+        :return:
+        """
+        return self
+    # end __iter__
+
+    # Next element
+    def next(self):
+        """
+        Next element
+        :return:
+        """
+        pass
+    # end next
 
     ########################################
     # Static
--- core/embeddings/Wordsim353.py
+++ core/embeddings/Wordsim353.py
@@ -1,10 +1,11 @@
 
 # Imports
 import csv
+from .WordSimilarity import WordSimilarity
 
 
 # Word similarity dataset
-class Wordsim353(object):
+class Wordsim353(WordSimilarity):
     """
     Word Similarity dataset 353
     """
@@ -14,6 +15,8 @@ class Wordsim353(object):
         """
         Constructor
         """
+        super(Wordsim353, self).__init__()
+
         # Words
         self._words = dict()
 
@@ -22,17 +25,20 @@ class Wordsim353(object):
             # Info
             word1 = row[0]
             word2 = row[1]
-            similarity = float(row[2])
 
             # Skip header
             if word1 != "Word 1":
+                similarity = float(row[2])
+
                 # Create dict for word 1 if necessary
                 if word1 not in self._words.keys():
+                    self._n_words += 1
                     self._words[word1] = dict()
                 # end if
 
                 # Create dict for word 2 if necessary
                 if word2 not in self._words.keys():
+                    self._n_words += 1
                     self._words[word2] = dict()
                 # end if
 
@@ -61,6 +67,56 @@ class Wordsim353(object):
 
         return None
     # end similarity
+
+    # Get similar words
+    def get_similar_words(self, word, limit=10):
+        """
+        Get similar words
+        :param word:
+        :param limit:
+        :return:
+        """
+        similar_words = list()
+
+        # For each words
+        for word2 in self._words[word].keys():
+            similar_words.append((word2, self._words[word][word2]))
+        # end for
+
+        # Sort
+        similar_words.sort(key=lambda tup: tup[1])
+
+        return similar_words[:limit]
+    # end get_similar_words
+
+    # Get list of words
+    def words(self):
+        """
+        Get list of words
+        :return:
+        """
+        return self._words.keys()
+    # end words
+
+    ########################################
+    # Override
+    ########################################
+
+    # Next element
+    def next(self):
+        """
+        Next element
+        :return:
+        """
+        if self._word_pos >= self._n_words:
+            self._word_pos = 0
+            raise StopIteration()
+        else:
+            word = self._words.keys()[self._word_pos]
+            self._word_pos += 1
+            return word, self.get_similar_words(word, limit=1000000)
+        # end if
+    # end next
 
     ########################################
     # Static
